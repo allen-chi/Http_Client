c9998837aeb9e24204bf9d665737f50187dd49a0
Ems <Ems>
2016-11-29 14:12:29 +0100

    udated

--- httpClientLibrary.js
+++ httpClientLibrary.js
@@ -1,12 +1,12 @@
 var request = require('request');
 
 var options = {  
-    url: 'https://www.reddit.com/r/funny.json',
+    url: 'http://api.wordnik.com:80/v4/word.json/Andela/examples?includeDuplicates=true&useCanonical=true&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
     method: 'GET',
     headers: {
         'Accept': 'application/json',
         'Accept-Charset': 'utf-8',
-        'User-Agent': 'my-reddit-client'
+        
     }
 };
 
