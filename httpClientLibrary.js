fc0ab8a8f0a76fa8f44ee6bb233726c6c4933a79
Ems <Ems>
2016-11-29 13:58:27 +0100

    updated

--- httpClientLibrary.js
+++ httpClientLibrary.js
@@ -1,1 +1,16 @@
+var request = require('request');
 
+var options = {  
+    url: 'https://www.reddit.com/r/funny.json',
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8',
+        'User-Agent': 'my-reddit-client'
+    }
+};
+
+request(options, function(err, res, body) {  
+    let json = JSON.parse(body);
+    console.log(json);
+});
