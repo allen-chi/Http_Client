d81251d4855a90a1524f571f5fd18c5ddc42bc88
Ems <Ems>
2016-11-30 03:12:16 +0100

    update

--- httpClientLibrary.js
+++ httpClientLibrary.js
@@ -1,16 +1,62 @@
-var request = require('request');
-
-var options = {  
-    url: 'http://api.wordnik.com:80/v4/word.json/Andela/examples?includeDuplicates=true&useCanonical=true&skip=0&limit=5&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5',
-    method: 'GET',
-    headers: {
-        'Accept': 'application/json',
-        'Accept-Charset': 'utf-8',
-        
-    }
-};
-
-request(options, function(err, res, body) {  
-    let json = JSON.parse(body);
-    console.log(json);
+var axios = require('axios');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+rl.setPrompt('define> ');
+rl.prompt();
+
+rl.write(`define allows you to lookup dictionary words` + `\n` +  `and their meaning directly from your console.` + `\n` + `Type ".exit" to kill process!` + `\n`);
+
+
+rl.question(`\n` + `Type 'word' to get definition` + `\n`, (word) => {
+    var word = word.toLowerCase();
+
+    if (word === "exit") {
+      rl.write(`Application exited.`);
+      process.exit();
+    }
+
+    rl.write(`Getting definition for 'word' ${word}` + `\n`);
+    getWordDefinition(word)
+        .then(function(response){
+            displayDefintion(response.data)
+        })
+
+        .catch(function(response){
+            wordDefinitionError(response);
+        })
+});
+
+
+
+function displayDefintion(jsonResponse) {
+  var numberFound = jsonResponse.length;
+
+  rl.write(`Found ${numberFound} result(s): ` + `\n`);
+
+  jsonResponse.forEach(function(item, idx) {
+    rl.write(`${idx + 1}` + `\n`);
+    rl.write(`Part of Speech:  ${item.partOfSpeech}` + `\n`);
+    rl.write(`Definition: ${item.text}` + `\n` + `\t`);
+  });
+}
+
+function getWordDefinition(word) {
+    return axios.get(`http://api.wordnik.com:80/v4/word.json/${word}/definitions`, {
+    params: {
+      limit: 200,
+      includeRelated: true,
+      useCanonical: false,
+      includeTags: false,
+      api_key: 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+    }
+  })
+}
+    
+
+function wordDefinitionError(response) {
+  rl.write(`Error getting word defintion please check word spelling.`);
+}
